ea2dacff724eb0a3d61728722296f79a631e0f84
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-10-31 07:16:07 +0300

    Problem #65

--- Amazon/Problem 65/Solution.java
+++ Amazon/Problem 65/Solution.java
@@ -1,42 +1,8 @@
 /**
- * Given a N by M matrix of numbers, print out the matrix in a clockwise spiral.
- *
- * For example, given the following matrix:
- *
- * <pre>
- * [[1,  2,  3,  4,  5],
- *  [6,  7,  8,  9,  10],
- *  [11, 12, 13, 14, 15],
- *  [16, 17, 18, 19, 20]]
- *  </pre>
- * You should print out the following:
- * <pre>
- * 1
- * 2
- * 3
- * 4
- * 5
- * 10
- * 15
- * 20
- * 19
- * 18
- * 17
- * 16
- * 11
- * 6
- * 7
- * 8
- * 9
- * 14
- * 13
- * 12
- * </pre>
- *
  * @author Oleg Cherednik
  * @since 31.10.2018
  */
-public class Problem65 {
+public class Solution {
 
     public static void main(String... args) {
         int[][] matrix = {
@@ -79,7 +45,6 @@ public class Problem65 {
             public int moveX(int x) {
                 return ++x;
             }
-
         },
         DOWN {
             public boolean isNextMoveAvailable(int x, int y, boolean[][] visited) {
