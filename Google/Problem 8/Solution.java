01a320902643ad0282921eb6c02f6071e9342715
Oleg Cherednik <oleg.cherednik@gmail.com>
2018-12-08 00:21:45 +0300

    Problem #8

---
+++ Google/Problem 8/Solution.java
@@ -0,0 +1,63 @@
+/**
+ * @author Oleg Cherednik
+ * @since 07.12.2018
+ */
+public class Solution {
+
+    public static void main(String... args) {
+        Node node = createTree();
+        System.out.println(countUnivalSubtrees(node));
+    }
+
+    private static Node createTree() {
+        Node a = new Node(1);
+        a.left = new Node(1);
+        a.right = new Node(1);
+
+        Node b = new Node(0);
+        b.left = a;
+        b.right = new Node(0);
+
+
+        Node root = new Node(0);
+        root.left = new Node(1);
+        root.right = b;
+
+        return root;
+    }
+
+    private static class Node {
+
+        private final int val;
+        private Node left;
+        private Node right;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static int countUnivalSubtrees(Node node) {
+        if (node == null)
+            return 0;
+
+        int count = countUnivalSubtrees(node.left);
+        count += countUnivalSubtrees(node.right);
+
+        if (isUnivalTree(node))
+            count++;
+
+        return count;
+    }
+
+    private static boolean isUnivalTree(Node node) {
+        if (node == null)
+            return true;
+        if (node.left != null && node.left.val != node.val)
+            return false;
+        if (node.right != null && node.right.val != node.val)
+            return false;
+        return isUnivalTree(node.left) && isUnivalTree(node.right);
+    }
+
+}
