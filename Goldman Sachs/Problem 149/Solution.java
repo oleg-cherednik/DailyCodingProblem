42f55940bffd5d8da1637f4db55f3050cb67cd4a
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-01-16 08:19:33 +0300

    Problem #149

---
+++ Goldman Sachs/Problem 149/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Oleg Cherednik
+ * @since 16.01.2019
+ */
+public class Solution {
+
+    public static void main(String... args) {
+        System.out.println(sum(1, 3)); // 5
+    }
+
+    private static final Map<Integer, Long> SUM = new HashMap<>();
+
+    static {
+        int i = 0;
+        long sum = 0;
+
+        for (int num : Arrays.asList(1, 2, 3, 4, 5, 6))
+            SUM.put(i++, sum += num);
+    }
+
+    public static long sum(int i, int j) {
+        return SUM.getOrDefault(j - 1, 0L) - SUM.getOrDefault(i - 1, 0L);
+    }
+
+}
