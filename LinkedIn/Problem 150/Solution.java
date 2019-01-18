29a7659855628fe7c8c4e7beb171bd613209d5b1
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-01-18 07:58:40 +0300

    Problem #150

--- LinkedIn/Problem 150/Solution.java
+++ LinkedIn/Problem 150/Solution.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * @author Oleg Cherednik
@@ -15,7 +17,27 @@ public class Solution {
         getClosest(points, new Point(1, 2), 2).forEach(point -> System.out.println(point.x + ":" + point.y));
     }
 
+    public static List<Point> getClosest(List<Point> points, Point centralPoint, int k) {
+        final Function<Point, Integer> getDistance = point -> (point.x - centralPoint.x) * (point.x - centralPoint.x)
+                + (point.y - centralPoint.y) * (point.y - centralPoint.y);
+        final Comparator<Point> sortByDistanceDesc = (one, two) -> Integer.compare(getDistance.apply(two), getDistance.apply(one));
+
+        Queue<Point> queue = new PriorityQueue<>(sortByDistanceDesc);
+
+        points.forEach(point -> {
+            if (queue.size() < k)
+                queue.add(point);
+            else if (sortByDistanceDesc.compare(queue.element(), point) < 0) {
+                queue.remove();
+                queue.add(point);
+            }
+        });
+
+        return new ArrayList<>(queue);
+    }
+
     private static final class Point {
+
         private final int x;
         private final int y;
 
@@ -23,15 +45,6 @@ public class Solution {
             this.x = x;
             this.y = y;
         }
-    }
-
-    public static List<Point> getClosest(List<Point> points, Point centralPoint, int k) {
-        final Function<Point, Integer> getDistance = point -> (point.x - centralPoint.x) * (point.x - centralPoint.x)
-                + (point.y - centralPoint.y) * (point.y - centralPoint.y);
-        return points.stream()
-                .sorted(Comparator.comparingInt(getDistance::apply))
-                .limit(k)
-                .collect(Collectors.toList());
     }
 
 }
