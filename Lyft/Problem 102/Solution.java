87f7aa0567c53ec3a341f1435c69015af3a4d0f3
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-01-18 19:24:48 +0300

    Problem #102

---
+++ Lyft/Problem 102/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+/**
+ * @author Oleg Cherednik
+ * @since 18.01.2019
+ */
+public class Solution {
+
+    public static void main(String... args) {
+        System.out.println(Arrays.toString(getSum(new int[]{1, 2, 3, 4, 5}, 9)));
+    }
+
+    public static int[] getSum(int[] arr, int k) {
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+
+        for (; j <= arr.length && sum != k; ) {
+            if (sum < k)
+                sum += arr[j++];
+            else
+                sum -= arr[i++];
+        }
+
+        int[] res = new int[j - i];
+        System.arraycopy(arr, i, res, 0, res.length);
+
+        return res;
+    }
+
+}
