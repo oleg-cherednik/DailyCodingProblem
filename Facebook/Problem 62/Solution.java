47a0188ef019fdec200335e431aab7fe40e2e320
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-02-04 17:57:48 +0300

    Problem #62

--- Facebook/Problem 62/Solution.java
+++ Facebook/Problem 62/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @author Oleg Cherednik
  * @since 04.02.2019
@@ -7,19 +9,16 @@ public class Solution {
     public static void main(String... args) {
         System.out.println(countTotalWays(2, 2));    // 2
         System.out.println(countTotalWays(3, 4));    // 10
+        System.out.println(countTotalWays(4, 4));    // 20
     }
 
     public static int countTotalWays(int N, int M) {
         int[] arr = new int[M];
+        Arrays.fill(arr, 1);
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (i == 0)
-                    arr[j] = 1;
-                else
-                    arr[j] += j == 0 ? 0 : arr[j - 1];
-            }
-        }
+        for (int i = 1; i < N; i++)
+            for (int j = 0; j < M; j++)
+                arr[j] += j == 0 ? 0 : arr[j - 1];
 
         return arr[M - 1];
     }
