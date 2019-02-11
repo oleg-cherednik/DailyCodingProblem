65a8ab59c8dbb0799c78f4007277cd14c45fefa6
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-02-11 09:38:42 +0300

    Problem #98

---
+++ Coursera/Problem 98/Solution.java
@@ -0,0 +1,68 @@
+/**
+ * @author Oleg Cherednik
+ * @since 11.02.2019
+ */
+public class Solution {
+
+    public static void main(String... args) {
+        System.out.println(exists(createBoard(), "ABCCED"));    // true
+        System.out.println(exists(createBoard(), "SEE"));       // true
+        System.out.println(exists(createBoard(), "ABCB"));      // false
+    }
+
+    private static char[][] createBoard() {
+        return new char[][] {
+                { 'A', 'B', 'C', 'E' },
+                { 'S', 'F', 'C', 'S' },
+                { 'A', 'D', 'E', 'E' }, };
+    }
+
+    public static boolean exists(char[][] board, String str) {
+        boolean[][] visited = createVisited(board);
+
+        for (int row = 0; row < board.length; row++)
+            for (int col = 0; col < board[row].length; col++)
+                if (exists(board, row, col, str, 0, visited))
+                    return true;
+
+        return false;
+    }
+
+    private static boolean[][] createVisited(char[][] board) {
+        boolean[][] visited = new boolean[board.length][];
+
+        for (int row = 0; row < board.length; row++)
+            visited[row] = new boolean[board[row].length];
+
+        return visited;
+    }
+
+    private static boolean exists(char[][] board, int row, int col, String str, int offs, boolean[][] visited) {
+        if (offs >= str.length())
+            return true;
+        if (row < 0 || row >= board.length)
+            return false;
+        if (col < 0 || col >= board[row].length)
+            return false;
+        if (visited[row][col])
+            return false;
+        if (board[row][col] != str.charAt(offs))
+            return false;
+
+        visited[row][col] = true;
+
+        if (exists(board, row, col + 1, str, offs + 1, visited))
+            return true;
+        if (exists(board, row, col - 1, str, offs + 1, visited))
+            return true;
+        if (exists(board, row + 1, col, str, offs + 1, visited))
+            return true;
+        if (exists(board, row - 1, col, str, offs + 1, visited))
+            return true;
+
+        visited[row][col] = false;
+        return false;
+    }
+
+
+}
