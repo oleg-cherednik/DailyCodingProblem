b87ba3a9f1a747cc3e944bf77d60900af374bf5c
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-02-12 18:14:04 +0300

    Problem #126

--- Facebook/Problem 126/Solution.java
+++ Facebook/Problem 126/Solution.java
@@ -8,9 +8,9 @@ public class Solution {
 
     public static void main(String... args) {
         int[] arr = { 1, 2, 3, 4, 5, 6 };
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));   // [1, 2, 3, 4, 5, 6]
         rotate(arr, 2);
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));   // [3, 4, 5, 6, 1, 2]
     }
 
     public static void rotate(int[] arr, int k) {
