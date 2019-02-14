51ef71debb7cd1849aab600bcaa9964e5ee9942e
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-02-14 17:22:03 +0300

    Problem #138

---
+++ Google/Problem 138/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+/**
+ * @author Oleg Cherednik
+ * @since 14.02.2019
+ */
+public class Solution {
+
+    public static void main(String... args) {
+        System.out.println(getMinCoinsAmount(16));
+    }
+
+    private static final int[] DENOMINATION = {1, 5, 10, 25};
+
+    public static int getMinCoinsAmount(int n) {
+        int[] cache = new int[n + 1];
+        Arrays.fill(cache, Integer.MAX_VALUE);
+
+        for (int d : DENOMINATION)
+            if (d < cache.length)
+                cache[d] = 1;
+
+        for (int i = 1; i < cache.length; i++)
+            for (int d : DENOMINATION)
+                if (i - d > 0)
+                    cache[i] = Math.min(cache[i], 1 + cache[i - d]);
+
+        return cache[n];
+    }
+}
