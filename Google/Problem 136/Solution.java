ce3281a658fe985b6d37def6cd650671b3da97ef
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-02-14 16:21:45 +0300

    Problem #136

--- Google/Problem 136/Solution.java
+++ Google/Problem 136/Solution.java
@@ -6,40 +6,44 @@ public class Solution {
 
     public static void main(String... args) {
         int[][] matrix = {
-                { 1, 1, 0, 0 },
-                { 1, 0, 1, 1 },
-                { 1, 0, 1, 1 },
-                { 0, 1, 0, 0 } };
+                {1, 1, 0, 0},
+                {1, 0, 1, 1},
+                {1, 0, 1, 1},
+                {0, 1, 0, 0}};
         System.out.println(getLargestAreaSet(matrix));   // 4
     }
 
     public static int getLargestAreaSet(int[][] matrix) {
-        int max = 0;
-        int[] arr = new int[matrix[0].length];
+        int maxArea = 0;
+        int[] cache = new int[matrix[0].length];
 
         for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < arr.length; col++) {
+            for (int col = 0; col < matrix[row].length; col++) {
                 if (matrix[row][col] == 0)
-                    arr[col] = 0;
-                else {
-                    int tmp = get(arr, col - 1) + 1;
-
-                    if (arr[col] == 0)
-                        arr[col] = tmp;
-                    else
-                        arr[col] *= 2;
-                }
-
-                max = Math.max(max, arr[col]);
+                    cache[col] = 0;
+                else
+                    cache[col]++;
             }
+
+            maxArea = Math.max(maxArea, inferArea(cache));
         }
 
-        return max;
+        return maxArea;
     }
 
-    private static int get(int[] arr, int col) {
-        return col < 0 || col >= arr.length ? 0 : arr[col];
+    private static int inferArea(int[] cache) {
+        int area = 0;
+
+        for (int i = 0; i < cache.length; i++) {
+            if (cache[i] == 0)
+                continue;
+
+            int min = cache[i];
+
+            for (int j = i; j < cache.length && cache[j] != 0; j++)
+                area = Math.max(area, Math.min(min, cache[j]) * (j - i + 1));
+        }
+
+        return area;
     }
-
-
 }
