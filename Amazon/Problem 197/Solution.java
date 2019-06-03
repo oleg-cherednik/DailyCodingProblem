7b1f869ab372cbb8bf38bf516b79367cc898700d
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-06-03 21:29:58 +0300

    Problem #197

---
+++ Amazon/Problem 197/Solution.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+/**
+ * @author Oleg Cherednik
+ * @since 3.06.2019
+ */
+public class Solution {
+
+    public static void main(String... args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        rotateRight(arr, 2);
+        System.out.println(Arrays.toString(arr));   // { 4, 5, 1, 2, 3 }
+    }
+
+    public static void rotateRight(int[] arr, int k) {
+        k %= arr.length;
+
+        reverse(arr, 0, arr.length - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, arr.length - 1);
+    }
+
+    private static void reverse(int[] arr, int l, int r) {
+        while (l < r)
+            swap(arr, l++, r--);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+}
