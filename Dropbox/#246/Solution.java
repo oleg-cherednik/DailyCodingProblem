8b58e0f24e4402894e7dc222cdd306755a7eda0b
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-05-24 15:11:06 +0300

    Dropbox

---
+++ Dropbox/#246/Solution.java
@@ -0,0 +1,41 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author Oleg Cherednik
+ * @since 22.05.2020
+ */
+public class Solution {
+
+    public static void main(String... args) {
+        System.out.println(isBalanced("([])[]({})"));   // true
+        System.out.println(isBalanced("([)]"));   // false
+        System.out.println(isBalanced("((()"));   // false
+    }
+
+    public static boolean isBalanced(String str) {
+        Deque<Character> stack = new LinkedList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == '(' || ch == '[' || ch == '{')
+                stack.push(ch);
+            else {
+                if (stack.isEmpty())
+                    return false;
+
+                char prv = stack.pop();
+
+                if (prv == '(' && ch != ')')
+                    return false;
+                if (prv == '[' && ch != ']')
+                    return false;
+                if (prv == '{' && ch != '}')
+                    return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
